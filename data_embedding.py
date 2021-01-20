b732790ddf37c22d288fe657bd43a66111e52055
jbgour <jeanbaptiste.gourlet@hotmail.fr>
2021-01-20 21:22:55 +0100

    added multiprocess

--- data_embedding.py
+++ data_embedding.py
@@ -2,40 +2,62 @@ from music21 import converter, instrument, note, chord
 import glob
 import pickle
 import pandas as pd
+import multiprocessing
 
-notes = []
-log = []
 
-for file in glob.glob("data/2011/*"):
-    midi = converter.parse(file)
+def embedding(year):
+    notes = []
+    log = []
+    folder_name = 'data/' + str(year) + '/*'
+    n = len(glob.glob(folder_name))
+    counter = 0
+    for file in glob.glob(folder_name):
+        midi = converter.parse(file)
 
-    print("Parsing %s" % file)
+        print("Parsing %s" % file)
+        print(str(year) + ' : ' + str(counter) + '/' + str(n))
 
-    notes_to_parse = None
+        notes_to_parse = None
 
-    notesInt = []
+        notesInt = []
 
-    try:  # file has instrument parts
-        s2 = instrument.partitionByInstrument(midi)
-        i = 0
-        notes_to_parse = []
-        while len(notes_to_parse) == 0 and i < len(s2.parts):
-            notes_to_parse = s2.parts[2].recurse()
+        try:  # file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            i = 0
+            notes_to_parse = []
+            while len(notes_to_parse) == 0 and i < len(s2.parts):
+                notes_to_parse = s2.parts[2].recurse()
 
-    except:  # file has notes in a flat structure
-        notes_to_parse = midi.flat.notes
+        except:  # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
 
-    for element in notes_to_parse:
-        if isinstance(element, note.Note):
-            notesInt.append(str(element.pitch))
-        elif isinstance(element, chord.Chord):
-            notesInt.append('.'.join(str(n.pitch) for n in element.notes))
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notesInt.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notesInt.append('.'.join(str(n.pitch) for n in element.notes))
 
-    notes += notesInt
-    log.append(file + ':' + str(len(notesInt)))
+        notes += notesInt
+        log.append(file + ':' + str(len(notesInt)))
 
-    with open('data/notes', 'wb') as filepath:
-        pickle.dump(notes, filepath)
+        with open('data/notes_' + str(year), 'wb') as filepath:
+            pickle.dump(notes, filepath)
 
-    with open('data/log', 'wb') as filepath:
-        pickle.dump(log, filepath)
+        with open('data/log_' + str(year), 'wb') as filepath:
+            pickle.dump(log, filepath)
+
+        counter += 1
+
+
+year_list = [2002, 2004, 2006, 2008, 2009, 2011, 2013, 2014, 2015, 2017, 2018]
+
+processes = [ ]
+for y in year_list:
+    t = multiprocessing.Process(target=embedding, args=(y,))
+    processes.append(t)
+    t.start()
+
+for one_process in processes:
+    one_process.join()
+
+print("Done!")
