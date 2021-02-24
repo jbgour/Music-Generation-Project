bc0aa8afe4aeca59b4174e0d9b025efcca35327d
jbgour <jeanbaptiste.gourlet@hotmail.fr>
2021-02-24 10:21:50 +0100

    clean data_embedding

--- data_embedding.py
+++ data_embedding.py
@@ -2,7 +2,7 @@ from music21 import converter, instrument, note, chord
 import glob
 import pickle
 import multiprocessing
-
+import os
 
 def embedding(year):
     """
@@ -39,17 +39,56 @@ def embedding(year):
 
         notes += notesInt
 
-        with open('data/notes_' + str(year), 'wb') as filepath:
+        with open('data/fake_notes_' + str(year), 'wb') as filepath:
             pickle.dump(notes, filepath)
 
         counter += 1
 
 
+def embedding_by_file(year):
+    """
+    embedding of MIDI files
+    :param year: int, year folder
+    :return: pickle file containing the pitches
+    """
+    folder_name = 'data/' + str(year) + '/*'
+    n = len(glob.glob(folder_name))
+    counter = 0
+    for file in glob.glob(folder_name):
+
+        midi = converter.parse(file)
+        print("Parsing %s" % file)
+        print(str(year) + ' : ' + str(counter) + '/' + str(n))
+        notes_to_parse = None
+        notesInt = []
+
+        try:  # file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            i = 0
+            notes_to_parse = []
+            while len(notes_to_parse) == 0 and i < len(s2.parts):
+                notes_to_parse = s2.parts[2].recurse()
+
+        except:  # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
+
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notesInt.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notesInt.append('.'.join(str(n.pitch) for n in element.notes))
+
+
+        with open('data/fake_notes_' + str(year) + '_' + str(counter), 'wb') as filepath:
+            pickle.dump(notesInt, filepath)
+
+        counter += 1
+
 def full_embedding():
     """
     parallelization of the embedding phase
     """
-    msg = input("do you reaaly want to run the embedding (y/n)")
+    msg = input("do you really want to run the embedding (y/n)")
 
     if msg == 'y':
 
@@ -64,13 +103,12 @@ def full_embedding():
             one_process.join()
 
         print("Done!")
-
     else:
 
         print('aborting')
 
 
 if __name__ == '__main__':
-    full_embedding()
+    embedding_by_file(2002)
 
 
